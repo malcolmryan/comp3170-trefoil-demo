2be4fbaada5a486128206aeaf026d40d47688964
Malcolm Ryan <malcolm.ryan@gmail.com>
2021-04-03 21:15:39 +1100

    Calculating tangents

--- src/comp3170/demos/trefoil/sceneobjects/Trefoil.java
+++ src/comp3170/demos/trefoil/sceneobjects/Trefoil.java
@@ -1,5 +1,6 @@
 package comp3170.demos.trefoil.sceneobjects;
 
+import org.joml.Vector3f;
 import org.joml.Vector4f;
 
 import com.jogamp.opengl.GL;
@@ -12,15 +13,23 @@ public class Trefoil extends SceneObject {
 	
 	private static final float TAU = (float) (Math.PI * 2);
 	
-	private static final int NSLICES = 1000;
+	private static final int NSLICES = 100;
+			
 	private Vector4f[] vertices;
 	private int vertexBuffer;
+
+	private int[] indices;
+
+	private int indexBuffer;
 	
 	public Trefoil(Shader shader) {
 		super(shader);
 
-		this.vertices = new Vector4f[NSLICES];
+		this.vertices = new Vector4f[NSLICES * 2];
+
+		Vector4f kAxis = new Vector4f(0,0,0,0);
 		
+		int k = 0;
 		for (int i = 0; i < NSLICES; i++) {
 			float t = i * TAU / NSLICES;  // [0, TAU)
 			
@@ -29,12 +38,34 @@ public class Trefoil extends SceneObject {
 			v.x = (float) (Math.sin(t) + 2 * Math.sin(2 * t));
 			v.y = (float) (Math.cos(t) - 2 * Math.cos(2 * t));
 			v.z = (float) -Math.sin(3*t);
+
+			vertices[k++] = v;
+
+			kAxis.x = (float) (Math.cos(t) + 4 * Math.cos(2 * t));
+			kAxis.y = (float) (-Math.sin(t) + 4 * Math.sin(2 * t));
+			kAxis.z = (float) (3 * Math.cos(3*t));		
+			kAxis.normalize3();
 			
-			vertices[i] = v;
+			Vector4f v2 = new Vector4f(v);
+			v2.add(kAxis);
+			
+			vertices[k++] = v2;
+			
 		}
 		
 		this.vertexBuffer = shader.createBuffer(vertices);		
 
+		// index buffer contains one line for each slice
+		// each line is [i, i+1]
+		this.indices = new int[2 * NSLICES];
+		
+		for (int i = 0; i < NSLICES; i++) {
+			indices[2*i] = 2*i;
+			indices[2*i+1] = 2*i+1;
+		}
+		
+		this.indexBuffer = shader.createIndexBuffer(indices);
+		
 	}
 
 	@Override
@@ -48,7 +79,9 @@ public class Trefoil extends SceneObject {
 		shader.setUniform("u_colour", colour);
 		shader.setAttribute("a_position", vertexBuffer);		
 
-        gl.glDrawArrays(GL.GL_POINTS, 0, vertices.length);           	
+		gl.glBindBuffer(GL.GL_ELEMENT_ARRAY_BUFFER, indexBuffer);
+		gl.glDrawElements(GL.GL_LINES, indices.length, GL.GL_UNSIGNED_INT, 0);		
+
 	}
 	
 	
