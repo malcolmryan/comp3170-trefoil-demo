3cb5630637e3d4ad00b055c3f7ec0ef01b2e2fd5
Malcolm Ryan <malcolm.ryan@gmail.com>
2021-04-03 22:46:47 +1100

    Added vertex colouring shader

---
+++ src/comp3170/demos/trefoil/colourVertex.glsl
@@ -0,0 +1,16 @@
+#version 410
+
+in vec4 a_position;	// vertex position as a homogeneous 3D point in model 
+in vec3 a_colour;	// RGB 
+
+uniform mat4 u_modelMatrix;			// MODEL -> WORLD
+uniform mat4 u_viewMatrix;			// WORLD -> VIEW
+uniform mat4 u_projectionMatrix;	// VIEW -> NDC
+
+out vec3 v_colour; // RGB
+
+void main() {
+	v_colour = a_colour;
+    gl_Position = u_projectionMatrix * u_viewMatrix * u_modelMatrix * a_position;
+}
+
