8e3bb939641ef6045c963c525fd5e69151d8d036
Malcolm Ryan <malcolm.ryan@gmail.com>
2021-05-29 17:09:23 +1000

    Added textures

---
+++ src/comp3170/demos/trefoil/textures/TextureLibrary.java
@@ -0,0 +1,50 @@
+package comp3170.demos.trefoil.textures;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+
+import com.jogamp.opengl.GL;
+import com.jogamp.opengl.GL4;
+import com.jogamp.opengl.GLContext;
+import com.jogamp.opengl.util.texture.Texture;
+import com.jogamp.opengl.util.texture.TextureIO;
+
+public class TextureLibrary {
+	
+	final private static File DIRECTORY = new File("src/comp3170/demos/trefoil/textures"); 
+
+	private final static Map<String, Integer> loadedTextures = new HashMap<String, Integer>();
+	
+	/**
+	 * Load a texture from a file in the textures directory.
+	 * 
+	 * @param filename	The name of the file to use
+	 * @return The texture ID of the loaded texture.
+	 * @throws IOException if the file can't be found.
+	 */
+	
+	public static int loadTexture(String filename) throws IOException {
+		GL4 gl = (GL4) GLContext.getCurrentGL();
+
+		// check whether it has already been loaded, if so, return that textureID
+		if (loadedTextures.containsKey(filename)) {
+			return loadedTextures.get(filename);
+		}
+		
+		File textureFile = new File(DIRECTORY, filename);
+		
+		Texture tex = TextureIO.newTexture(textureFile, true);
+		int textureID = tex.getTextureObject();
+		
+		gl.glBindTexture(GL.GL_TEXTURE_2D, textureID);
+		// TODO: Set texture parameters
+
+		// record this texture as having been loaded
+		loadedTextures.put(filename, textureID);
+		
+		return textureID;
+	}
+
+}
